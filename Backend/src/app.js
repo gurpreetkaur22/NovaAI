529e0157e551862ff5a6d11fb222bbcac9aa83e7
Gurpreet Kaur <112333393+gurpreetkaur22@users.noreply.github.com>
2025-10-04 22:55:10 +0530

    deploy fixes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 // middlewares
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://novaai-35p8.onrender.com'],
+    origin: ['http://localhost:5173', 'https://novaai-35p8.onrender.com', 'https://novaai-frontend.onrender.com'],
     credentials: true
 }));
 app.use(express.json());
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,5 +4,14 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173,
+    strictPort: true,
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  }
 })
